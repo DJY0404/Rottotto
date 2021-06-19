de8621aa9ef33c9b434ccdc1fc11f2812f4b0f9e
DJY0404 <55430286+DJY0404@users.noreply.github.com>
2021-06-19 22:49:57 +0900

    Add files via upload

---
+++ src/main/java/com/example/dbimport/NumCountTable.java
@@ -0,0 +1,377 @@
+package com.example.dbimport;
+
+public class NumCountTable {
+    public int nc1,nc2,nc3,nc4,nc5,nc6,nc7,nc8,nc9,nc10,
+    nc11,nc12,nc13,nc14,nc15,nc16,nc17,nc18,nc19,nc20,
+    nc21,nc22,nc23,nc24,nc25,nc26,nc27,nc28,nc29,nc30,
+    nc31,nc32,nc33,nc34,nc35,nc36,nc37,nc38,nc39,nc40,
+    nc41,nc42,nc43,nc44,nc45,nc46;
+
+    public int getNc1() {
+        return nc1;
+    }
+
+    public void setNc1(int nc1) {
+        this.nc1 = nc1;
+    }
+
+    public int getNc2() {
+        return nc2;
+    }
+
+    public void setNc2(int nc2) {
+        this.nc2 = nc2;
+    }
+
+    public int getNc3() {
+        return nc3;
+    }
+
+    public void setNc3(int nc3) {
+        this.nc3 = nc3;
+    }
+
+    public int getNc4() {
+        return nc4;
+    }
+
+    public void setNc4(int nc4) {
+        this.nc4 = nc4;
+    }
+
+    public int getNc5() {
+        return nc5;
+    }
+
+    public void setNc5(int nc5) {
+        this.nc5 = nc5;
+    }
+
+    public int getNc6() {
+        return nc6;
+    }
+
+    public void setNc6(int nc6) {
+        this.nc6 = nc6;
+    }
+
+    public int getNc7() {
+        return nc7;
+    }
+
+    public void setNc7(int nc7) {
+        this.nc7 = nc7;
+    }
+
+    public int getNc8() {
+        return nc8;
+    }
+
+    public void setNc8(int nc8) {
+        this.nc8 = nc8;
+    }
+
+    public int getNc9() {
+        return nc9;
+    }
+
+    public void setNc9(int nc9) {
+        this.nc9 = nc9;
+    }
+
+    public int getNc10() {
+        return nc10;
+    }
+
+    public void setNc10(int nc10) {
+        this.nc10 = nc10;
+    }
+
+    public int getNc11() {
+        return nc11;
+    }
+
+    public void setNc11(int nc11) {
+        this.nc11 = nc11;
+    }
+
+    public int getNc12() {
+        return nc12;
+    }
+
+    public void setNc12(int nc12) {
+        this.nc12 = nc12;
+    }
+
+    public int getNc13() {
+        return nc13;
+    }
+
+    public void setNc13(int nc13) {
+        this.nc13 = nc13;
+    }
+
+    public int getNc14() {
+        return nc14;
+    }
+
+    public void setNc14(int nc14) {
+        this.nc14 = nc14;
+    }
+
+    public int getNc15() {
+        return nc15;
+    }
+
+    public void setNc15(int nc15) {
+        this.nc15 = nc15;
+    }
+
+    public int getNc16() {
+        return nc16;
+    }
+
+    public void setNc16(int nc16) {
+        this.nc16 = nc16;
+    }
+
+    public int getNc17() {
+        return nc17;
+    }
+
+    public void setNc17(int nc17) {
+        this.nc17 = nc17;
+    }
+
+    public int getNc18() {
+        return nc18;
+    }
+
+    public void setNc18(int nc18) {
+        this.nc18 = nc18;
+    }
+
+    public int getNc19() {
+        return nc19;
+    }
+
+    public void setNc19(int nc19) {
+        this.nc19 = nc19;
+    }
+
+    public int getNc20() {
+        return nc20;
+    }
+
+    public void setNc20(int nc20) {
+        this.nc20 = nc20;
+    }
+
+    public int getNc21() {
+        return nc21;
+    }
+
+    public void setNc21(int nc21) {
+        this.nc21 = nc21;
+    }
+
+    public int getNc22() {
+        return nc22;
+    }
+
+    public void setNc22(int nc22) {
+        this.nc22 = nc22;
+    }
+
+    public int getNc23() {
+        return nc23;
+    }
+
+    public void setNc23(int nc23) {
+        this.nc23 = nc23;
+    }
+
+    public int getNc24() {
+        return nc24;
+    }
+
+    public void setNc24(int nc24) {
+        this.nc24 = nc24;
+    }
+
+    public int getNc25() {
+        return nc25;
+    }
+
+    public void setNc25(int nc25) {
+        this.nc25 = nc25;
+    }
+
+    public int getNc26() {
+        return nc26;
+    }
+
+    public void setNc26(int nc26) {
+        this.nc26 = nc26;
+    }
+
+    public int getNc27() {
+        return nc27;
+    }
+
+    public void setNc27(int nc27) {
+        this.nc27 = nc27;
+    }
+
+    public int getNc28() {
+        return nc28;
+    }
+
+    public void setNc28(int nc28) {
+        this.nc28 = nc28;
+    }
+
+    public int getNc29() {
+        return nc29;
+    }
+
+    public void setNc29(int nc29) {
+        this.nc29 = nc29;
+    }
+
+    public int getNc30() {
+        return nc30;
+    }
+
+    public void setNc30(int nc30) {
+        this.nc30 = nc30;
+    }
+
+    public int getNc31() {
+        return nc31;
+    }
+
+    public void setNc31(int nc31) {
+        this.nc31 = nc31;
+    }
+
+    public int getNc32() {
+        return nc32;
+    }
+
+    public void setNc32(int nc32) {
+        this.nc32 = nc32;
+    }
+
+    public int getNc33() {
+        return nc33;
+    }
+
+    public void setNc33(int nc33) {
+        this.nc33 = nc33;
+    }
+
+    public int getNc34() {
+        return nc34;
+    }
+
+    public void setNc34(int nc34) {
+        this.nc34 = nc34;
+    }
+
+    public int getNc35() {
+        return nc35;
+    }
+
+    public void setNc35(int nc35) {
+        this.nc35 = nc35;
+    }
+
+    public int getNc36() {
+        return nc36;
+    }
+
+    public void setNc36(int nc36) {
+        this.nc36 = nc36;
+    }
+
+    public int getNc37() {
+        return nc37;
+    }
+
+    public void setNc37(int nc37) {
+        this.nc37 = nc37;
+    }
+
+    public int getNc38() {
+        return nc38;
+    }
+
+    public void setNc38(int nc38) {
+        this.nc38 = nc38;
+    }
+
+    public int getNc39() {
+        return nc39;
+    }
+
+    public void setNc39(int nc39) {
+        this.nc39 = nc39;
+    }
+
+    public int getNc40() {
+        return nc40;
+    }
+
+    public void setNc40(int nc40) {
+        this.nc40 = nc40;
+    }
+
+    public int getNc41() {
+        return nc41;
+    }
+
+    public void setNc41(int nc41) {
+        this.nc41 = nc41;
+    }
+
+    public int getNc42() {
+        return nc42;
+    }
+
+    public void setNc42(int nc42) {
+        this.nc42 = nc42;
+    }
+
+    public int getNc43() {
+        return nc43;
+    }
+
+    public void setNc43(int nc43) {
+        this.nc43 = nc43;
+    }
+
+    public int getNc44() {
+        return nc44;
+    }
+
+    public void setNc44(int nc44) {
+        this.nc44 = nc44;
+    }
+
+    public int getNc45() {
+        return nc45;
+    }
+
+    public void setNc45(int nc45) {
+        this.nc45 = nc45;
+    }
+
+    public int getNc46() {
+        return nc46;
+    }
+
+    public void setNc46(int nc46) {
+        this.nc46 = nc46;
+    }
+}
